38b2c81da7cdaabeb262f42b3ae3946307968f44
sofia11108 <sofia35025@gmail.com>
2023-05-20 21:34:58 -0500

    página apptastico con responsive

---
+++ js.js
@@ -0,0 +1,29 @@
+toggle.onclick = () => {
+    items.classList.toggle("open")
+    toggle.classList.toggle("close")
+}
+
+
+window.addEventListener('scroll', function () {
+  
+  let elementos = document.querySelectorAll('.caracteristicas')
+  
+    let scrollTop = document.documentElement.scrollTop;  //cantidad de scroll que se hace
+
+    for (let i = 0; i < elementos.length; i++) {
+      let alturaElemento = elementos[i].offsetTop;
+    
+      if(alturaElemento  - 300 < scrollTop) {
+        elementos[i].style.opacity = 1;
+       
+        if (i == 0) {
+          elementos[0].classList.add('desdeIzquierda')
+        } else if (i == 1) {
+          elementos[1].classList.add('desdeAbajo')
+        } else if (i == 2) {
+          elementos[2].classList.add('desdeDerecha')
+        }
+
+      }
+    }
+})
